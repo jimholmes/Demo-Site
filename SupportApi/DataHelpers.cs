675dedb833fe83acee0ac372a536a33eec6b0b98
Jim Holmes <jim@iterativerose.com>
2015-03-01 23:46:44 -0500

    Now using Faker-cs

--- SupportApi/DataHelpers.cs
+++ SupportApi/DataHelpers.cs
@@ -16,7 +16,6 @@ namespace SupportApi
 
         private static string conn_str =
             "Integrated Security=true;Initial Catalog=demos;Server=(local);";
-            //"Integrated Security=true;Initial Catalog=demos;Server=s12r2\\sqlexpress;";
         public static int get_high_contact_id()
         {
             int id;
@@ -40,16 +39,14 @@ namespace SupportApi
         public static int create_new_contact()
         {
             Contact contact = new Contact();
-            contact.Company = random_string(8);
-            contact.FName = random_string(6);
-            contact.LName = random_string(10);
-            contact.Region = random_string(4);
+            contact.Company = Faker.Company.BS();
+            contact.FName = Faker.Name.First();
+            contact.LName = Faker.Name.Last();
+            contact.Region = Faker.Address.Country();
 
             ContactRepository repos = new ContactRepository();
             return repos.SaveContact(contact).Id;
         }
-
-        // http://stackoverflow.com/questions/1122483/random-string-generator-returning-same-string
         private static Random random = 
             new Random((int)DateTime.Now.Ticks);
         private static string random_string(int size)
