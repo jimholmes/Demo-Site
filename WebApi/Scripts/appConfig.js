ea8f4a048b5b4a5624ef8c2b8aaf79b24d7fddd7
Jim Holmes <Jim@GuidepostSystems.com>
2018-04-27 12:08:48 -0700

    Moved target URL to appConfig file to eliminate duplication.

---
+++ WebApi/Scripts/appConfig.js
@@ -0,0 +1,14 @@
+﻿var appConfig = function () {
+    //private
+
+    var targetString = "http://demosite.com/api/contact/";
+       
+    return {
+        //public
+        target: targetString
+        //readUri: readConnectionString //,
+        // createUri: createConnectionString,
+        // updateUri: updateConnectionString,
+        // deleteUri: deleteConnectionString
+    }
+}();
